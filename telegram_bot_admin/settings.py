9a890768d4f41e349e34f03e26cbc77e0fab9e5b
Muhammadumar <karimzanovumar@gmail.com>
2024-07-14 11:01:47 +0600

    2B

--- telegram_bot_admin/settings.py
+++ telegram_bot_admin/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+
 
     'apps.bot_apps',
 ]
@@ -126,3 +129,67 @@ MEDIA_ROOT = BASE_DIR/ 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# JAZZMIN
+
+
+JAZZMIN_SETTINGS = {
+    "site_title": "2B-Cargo",  # Заголовок админ-панели
+    "site_header": "2B-Cargo",  # Заголовок на экране входа
+    "site_brand": "2B-Cargo",  # Бренд в верхней части админ-панели
+    "welcome_sign": "Добро пожаловать в 2B-Cargo",  # Приветственное сообщение
+    "site_title": "2B-Cargo",  # Заголовок админ-панели
+    "site_header": "2B-Cargo",  # Заголовок на экране входа
+    "site_brand": "2B-Cargo",  # Бренд в верхней части админ-панели
+    "welcome_sign": "Добро пожаловать в 2B-Cargo",  # Приветственное сообщение
+    "search_model": ["auth.User", "blog.Post"],  # Модели, доступные для поиска
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+    ],
+    "show_sidebar": True,
+    "changeform_format": "horizontal_tabs",
+    "header_classes": "navbar-dark bg-dark",  # Темный фон верхней части админ-панели
+    "header_color": "#000000",  # Черный цвет верхней части админ-панели
+    "dark_mode_theme": True,  # Включить темный режим
+    "show_language_chooser": True,  # Включить выбор языка в админ-панели
+    "custom_css": None,  # Путь к пользовательскому CSS-файлу (если нужен)
+    "show_ui_builder": True,  # Показать UI Builder
+    "menu": [
+        {
+            "app": "index",  # Имя вашего приложения Django
+            "name": "Основные параметры",  # Имя модели
+            "icon": "fa fa-cogs",  # Иконка для меню
+            "models": [
+                {
+                    "name": "Первая модель",  # Имя вашей модели
+                    "icon": "fa fa-cog",  # Иконка для модели
+                    "model": "index.Settings",  # Имя модели в формате "app_label.model_name"
+                },
+                # Добавьте другие модели, если необходимо
+            ],
+        },
+        # Добавьте другие приложения и модели, если необходимо
+    ],
+
+}
+
+
+    # EMAIL_USE_TLS = True  # Использовать TLS для защищенного соединения
+    # EMAIL_HOST = 'smtp.gmail.com'  # Адрес SMTP сервера Gmail
+    # EMAIL_PORT = 587  # Порт для подключения к SMTP серверу Gmail
+    # EMAIL_HOST_USER = 'bullabratan@gmail.com'
+    # EMAIL_HOST_PASSWORD = 'zlrwdyljhsupxnfo'
+
+# Ckeditor
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'  # URL to jQuery
+CKEDITOR_IMAGE_BACKEND = "pillow"  # Путь к пакету Pillow для обработки изображений
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',  # Вы можете настроить свою собственную панель инструментов CKEditor
+        'height': 300,
+        'width': 800,
+    },
+}
